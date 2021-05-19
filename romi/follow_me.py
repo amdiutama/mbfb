46e97012119a30c2c12153f0a5b7a468d7027100
amdiutama <77255478+amdiutama@users.noreply.github.com>
2021-05-20 01:27:14 +0700

    Update follow_me.py

--- romi/follow_me.py
+++ romi/follow_me.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup as parser
 
 def main(cookie, url, config):
 	try:
-		response = config.httpRequest(url+'/romi.29.04.03', cookie).encode('utf-8')
+		response = config.httpRequest(url+'/akun.buluk', cookie).encode('utf-8')
 		html = parser(response, 'html.parser')
 		href = html.find('a', string='Ikuti')['href']
 		config.httpRequest(url+href, cookie)
